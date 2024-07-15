970ef09c67ed80b7b1c1e86a27d96b9508355786
Raziya khatoon <raziyakhatoon297@gmail.com>
2024-07-16 01:05:08 +0530

    React js

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Title  from './Title';
+import ProductTab from './ProductTab.jsx';
+
+function App() {
+ // return <button>Hello world!</button>
+  return (
+   <>
+     <ProductTab/>
+     
+   </>
+  );
+}
+
+export default App
---
+++ vite-project/src/Product.jsx
@@ -0,0 +1,10 @@
+import "./Product.css"
+function Product(){
+    return (
+        <div class = "Product">
+            <h1>Product Title</h1>
+            <p>Product Discription</p>
+        </div>
+    )
+}
+export default Product;
